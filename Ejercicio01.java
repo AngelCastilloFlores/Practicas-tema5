55cef6bb51ae0e4c18509b21cbb6515f998f14de
Ángel Castillo Flores <akmusic74@gmail.com>
2016-11-13 19:21:28 +0100

    Add files via upload

---
+++ Ejercicio01.java
@@ -0,0 +1,19 @@
+//Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.
+
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		for (int i = 0; i <= 100; i += 5){
+			System.out.println(i);
+		}
+	}
+}
+
+	
+
+
